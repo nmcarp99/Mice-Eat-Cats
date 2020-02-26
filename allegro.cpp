e1d2464d00fb82b4aed71da8dc745548950b6d33
nmcarp99 <nmcarp99@gmail.com>
2020-02-26 15:54:00 -0500

    Revert "remove need for allegro5 installation"
    This reverts commit 90c5deed5e9d70faf24f49f3c313c349b322ef8a.

--- allegro.cpp
+++ allegro.cpp
@@ -1,11 +1,11 @@
 #include <string>
 #include <fstream>
 #include <iostream>
-#include <./allegro5/allegro_audio.h>
-#include <./allegro5/allegro_acodec.h>
-#include <./allegro5/allegro.h>
-#include <./allegro5/allegro_image.h>
-#include <./allegro5/allegro_primitives.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
 
 using namespace std;
 const float FPS = 60;
