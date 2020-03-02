a077445f320a1fff67185c68756b80137bfd7a49
nmcarp99 <nmcarp99@gmail.com>
2020-03-02 11:18:28 -0500

    reorder includes

--- Mice-Eat-Cats.cpp
+++ Mice-Eat-Cats.cpp
@@ -1,8 +1,8 @@
 #include <fstream>
 #include <iostream>
+#include <allegro5/allegro.h>
 #include <allegro5/allegro_audio.h>
 #include <allegro5/allegro_acodec.h>
-#include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_primitives.h>
 
